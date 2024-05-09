865e1340e7759884ab5914c83b203a704e0ec73d
Anthonyuff <anthonyrebello612@gmail.com>
2024-05-09 16:33:44 -0300

    LU

--- decompoLU.py
+++ decompoLU.py
@@ -18,10 +18,27 @@ def X(U,y):
     n=len(y)
     x=np.zeros(n)
     for i in range(n-1, -1, -1):
-        x[i] = (y[i] - np.dot(U[i, i+1:], x[i+1:])) / U[i, i]
+        if U[i, i] == 0:
+            
+            print("A matriz U é singular. O sistema pode não ter solução única.")
+
+            return None
+        x[i] = (y[i] - U[i, i+1:] @ x[i+1:]) / U[i, i]
     return x 
 
-A=np.array([[5,2,1],[3,1,4],[1,1,3]])
+#A=np.array([[5,2,1],[3,1,4],[1,1,3]])
+A=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
 L,U=decompoLU(A)
 
-print(L)
+#b=np.array([[0,-7,5]])
+
+b=np.array([2,-1,1])
+
+B=b.T
+
+y=Y(L,B)
+
+x=X(U,y)
+
+print(x)
